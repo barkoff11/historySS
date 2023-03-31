7d2b1cd780c9cf3ed3142fa89a473e958f7ebcb8
barkoff11 <117163248+barkoff11@users.noreply.github.com>
2023-04-01 02:06:48 +0300

    Add files via upload

---
+++ scrypt.js
@@ -0,0 +1,16 @@
+const openPopup = document.getElementById('open_pop_up');
+const closePopup = document.getElementById('pop_up_close');
+const popUp = document.getElementById('pop_up');
+
+
+openPopup.addEventListener('click', function (e) {
+    e.preventDefault();
+    popUp.classList.add('active');
+
+})
+
+closePopup.addEventListener('click', () => {
+    popUp.classList.remove('active');
+})
+
+
